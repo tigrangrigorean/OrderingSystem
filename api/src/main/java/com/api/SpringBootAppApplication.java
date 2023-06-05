1b5431c2cb6a61a00d9607fd1e959a3f763eda8f
= <tigran_grigoryan05@inbox.ru>
2023-06-05 20:46:47 +0400

    Update

--- api/src/main/java/com/api/SpringBootAppApplication.java
+++ api/src/main/java/com/api/SpringBootAppApplication.java
@@ -3,10 +3,7 @@ package com.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
+
 @SpringBootApplication
 public class SpringBootAppApplication {
 
